79924724dec757a9312be2faa338d4008b85587a
Nick Dolan <nickdolan.dev@gmail.com>
2022-10-20 11:11:25 +0300

    Add config

---
+++ config.py
@@ -0,0 +1,19 @@
+config = {
+    'exchange': 'Binance Perpetual Futures',
+    'import_start_date': '2022-08-01',
+    'corr_start_date': '2022-09-01',
+    'corr_finish_date': '2022-10-01',
+    'timeframe': '1D',
+    'pairs': [
+        'BTC-USDT',
+        'ETH-USDT',
+        'BNB-USDT',
+        'XRP-USDT',
+        'ADA-USDT',
+        'SOL-USDT',
+        'DOGE-USDT',
+        'MATIC-USDT',
+        'DOT-USDT',
+        'TRX-USDT'
+    ]
+}
